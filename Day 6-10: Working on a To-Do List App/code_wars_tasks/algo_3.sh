d6a2145632c5adb5c640163c296af22aa9f670c7
marksikaundi <emmanuelmark117@gmail.com>
2023-10-18 10:12:17 +0200

    changed the codes

--- Day 6-10: Working on a To-Do List App/code_wars_tasks/algo_3.sh
+++ Day 6-10: Working on a To-Do List App/code_wars_tasks/algo_3.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# Function to calculate the sum of multiples of 3 or 5 below a given number
 sum_of_multiples() {
     local number=$1
     local sum=0
 
-    if [ $number -le 0 ]; then
-        echo "0"
+    if [ -z "$number" ] || [ $number -le 0 ]; then
+        echo "The sum of multiples of 3 or 5 below $number is: 0"
         return
     fi
 
@@ -15,10 +16,11 @@ sum_of_multiples() {
         fi
     done
 
-    echo $sum
+    echo "The sum of multiples of 3 or 5 below $number is: $sum"
 }
 
+# Input number
 read -p "Enter a number: " num
 
-result=$(sum_of_multiples $num)
-echo "The sum of multiples of 3 or 5 below $num is: $result"
+# Call the function
+sum_of_multiples "$num"
